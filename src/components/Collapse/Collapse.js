12b7cd59f95e0fe6b046049d48d139765ca795ef
Melissa Touzeau <melissa_touzeau@hotmail.fr>
2023-02-21 10:43:52 +0100

    Commentaires

--- src/components/Collapse/Collapse.js
+++ src/components/Collapse/Collapse.js
@@ -4,18 +4,22 @@ import flecheHaute from "../../assets/fleche.png";
 import "../Collapse/Collapse.css";
 
 function Collapse(props) {
-  const [open, openSet] = useState(true);
+  const [open, openSet] = useState(true); // initialise l'état du collapse à fermé (=open)
 
   return (
     <div className="collapse">
       <div className="collapse_header" onClick={() => openSet(!open)}>
+        {/* change l'état : false ou true */}
         <span>{props.title}</span>
         {open && <img className="collapse_chevron" src={fleche} alt="Fleche" />}
+        {/* affichage conditionnel */}
         {!open && (
           <img className="collapse_chevron" src={flecheHaute} alt="Fleche" />
         )}
+        {/* open est false = collapse est ouvert */}
       </div>
       {!open && <div className="collapse_content">{props.children}</div>}
+      {/* gère l'affichage du contenu */}
     </div>
   );
 }
--- src/components/Slideshow/Slideshow.js
+++ src/components/Slideshow/Slideshow.js
@@ -4,13 +4,15 @@ import fleche_gauche from "../../assets/fleche_gauche.png";
 import "../Slideshow/Slideshow.css";
 
 function Slideshow(props) {
-  const [initialPicture, setPicture] = useState(0);
+  const [initialPicture, setPicture] = useState(0); // Initialise l'index de mon slide à 0
 
   const nextSlide = () => {
     if (initialPicture === props.img.length - 1) {
-      setPicture(0);
+      // -1 pour coller à l'index calculé à partir de 0.
+      // Vérifie si l'image affichée est la dernière de la slide
+      setPicture(0); // retourne à la premiere image
     } else {
-      setPicture(initialPicture + 1);
+      setPicture(initialPicture + 1); // affiche la photo suivante
     }
   };
 
@@ -25,14 +27,15 @@ function Slideshow(props) {
   return (
     <div className="slideshow_container">
       {props.img.map(
+        // affiche chaque image
         (picture, index) =>
-          index === initialPicture && (
+          index === initialPicture && ( // affichage conditionnel (true + expression = expression)
             <div className="slideshow_photos" key={index}>
               <img src={picture} alt={props.alt} />
             </div>
           )
       )}
-      {props.img.length > 1 ? (
+      {props.img.length > 1 ? ( // affiche les flèches et leur logique si le nombre d'image >1
         <>
           <div className="right_fleche" onClick={nextSlide}>
             <img src={fleche_droite} alt="Next slide"></img>
